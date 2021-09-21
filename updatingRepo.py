e45151ccc3b6f68a3c2a324a79b98e6b57d34d61
Nixon Chan <68513632+NixonCTChan@users.noreply.github.com>
2021-09-20 23:10:14 -0400

    token

--- updatingRepo.py
+++ updatingRepo.py
@@ -1,5 +1,5 @@
 import subprocess
 subprocess.call(['git', 'add', '-A'])
 subprocess.call(['git', 'commit', '-m', '{}'.format('routine update')])
-subprocess.call(['git', 'push', 'https://NixonCTChan:{}@github.com/NixonCTChan/Thermal.git'.format('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')])
+subprocess.call(['git', 'push', 'https://NixonCTChan:{}@github.com/NixonCTChan/Thermal.git'.format('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')])
 
