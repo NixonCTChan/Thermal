fc28f5684ca93fcc0f71a4a40ee82a4a588132ce
Nixon Chan <68513632+NixonCTChan@users.noreply.github.com>
2021-09-20 22:16:42 -0400

    fixing update

--- updatingRepo.py
+++ updatingRepo.py
@@ -1,6 +1,5 @@
 import subprocess
 subprocess.call(['git', 'add', '-A'])
 subprocess.call(['git', 'commit', '-m', '{}'.format('routine update')])
-subprocess.call(['git', 'push', 'origin'])
-subprocess.call(['NixonCTChan'])
-subprocess.call(['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'])
+subprocess.call(['git', 'push', 'https://{}@github.com/NixonCTChan/Thermal.git'.format('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')])
+
